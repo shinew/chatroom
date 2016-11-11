a2e2e9e3919e524d547e3d6334b6ef50eb3fbd5a
Shine Wang <shine.sw.wang@gmail.com>
2016-11-10 23:37:39 -0800

    got react working

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve('src/assets/js/main.js'),
+  entry: path.resolve('src/assets/js/main.jsx'),
   output: {
     path: path.resolve('public/js'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
+  module : {
+    loaders: [
+      {
+        test : /\.jsx?/,
+        include : path.resolve('.'),
+        loader : 'babel'
+      }
+    ]
+  }
 };
