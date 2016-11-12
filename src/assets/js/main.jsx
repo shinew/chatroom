ab653dd517f12da512f672be35c8778e54022814
Shine Wang <shine.sw.wang@gmail.com>
2016-11-12 03:28:28 -0800

    everything works! (multiple tabs)

--- src/assets/js/main.jsx
+++ src/assets/js/main.jsx
@@ -1,13 +1,119 @@
 // @flow
 
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
 
-function foo(a: number): number {
-  return 3;
+type MessageType = {
+  text: string;
+  timestamp: number;
+}
+
+const Message = (props: { message: MessageType }) => (
+  <p key={props.message.timestamp}>{props.message.text}</p>
+);
+
+const MessageList = (props: { messages: Array<MessageType>}) => (
+  <div>
+    {props.messages.map(m => Message({message: m}))}
+  </div>
+);
+
+class TextForm extends React.Component {
+  props: {
+    handleNewMessage: (message: MessageType) => void
+  };
+  state: {
+    value: string
+  };
+  handleChange: () => void;
+  _handleChange: () => void;
+  handleSubmit: () => void;
+  _handleSubmit: () => void;
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this._handleChange.bind(this);
+    this.handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _handleChange({target: { value }}: {target: { value: string } }) {
+    this.setState({value: value});
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault();
+    const timestamp = Date.now().valueOf();
+    this.props.handleNewMessage({
+      text: this.state.value,
+      timestamp: timestamp
+    });
+    this.setState({value: ''});
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+class ChatApp extends React.Component {
+  props: {
+    name: string
+  };
+  state: {
+    messages: Array<MessageType>
+  };
+  handleNewMessage: (message: MessageType) => void;
+  _handleNewMessage: (message: MessageType) => void;
+  handleNewMessageFromServer: (message: MessageType) => void;
+  _handleNewMessageFromServer: (message: MessageType) => void;
+  socket: any;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: []
+    };
+    this.handleNewMessage = this._handleNewMessage.bind(this);
+    this.handleNewMessageFromServer = this._handleNewMessageFromServer.bind(this);
+    this.socket = io.connect('http://localhost:8080');
+
+    this.socket.on('new message', (msg) => {
+      this.handleNewMessageFromServer(msg);
+    });
+  }
+
+  _handleNewMessage(message: MessageType) {
+    this.socket.emit('sent message', message);
+  }
+
+  _handleNewMessageFromServer(message: MessageType) {
+    this.setState(({ messages }: { messages: Array<MessageType> }) => {
+      messages.push(message);
+      return {
+        messages: messages
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+        <MessageList messages={this.state.messages} />
+        <TextForm handleNewMessage={this.handleNewMessage}/>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-  <h1>Hello, world!{foo(3)}</h1>,
+  <ChatApp name='My Chatroom' />,
   document.getElementById('react')
 );
--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(express.static('public'));
-app.get('*', (req, res) => {
-  res.sendStatus(500);
-})
-app.listen(3000);
+
+io.on('connection', (socket) => {
+  console.log('new user');
+  socket.on('sent message', (msg) => {
+    console.log('got new message');
+    io.emit('new message', msg);
+  });
+});
+
+server.listen(8080);
