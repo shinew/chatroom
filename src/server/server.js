6d5a404c2c0725c59eb586681e78e287f7da7758
Shine Wang <shine.sw.wang@gmail.com>
2016-12-18 20:19:11 -0800

    added bower + bootstrap

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,6 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
 
 app.use(express.static('public'));
-
-io.on('connection', (socket) => {
-  console.log('new user');
-  socket.on('sent message', (msg) => {
-    console.log('got new message');
-    io.emit('new message', msg);
-  });
-});
-
 server.listen(8080);
